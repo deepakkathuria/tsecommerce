ba9eb3c447351f047cb485ada24e4e4e804c9998
divyakathuriya <deepakkathuria32@gmail.com>
2023-12-20 12:13:25 +0530

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return (
+   <div>
+    <header>
+      Ts Amazon
+    </header>
+    <main>
+      main section
+    </main>
+    <footer>
+      All right reserved
+    </footer>
+
+   </div>
+  )
+}
+
+export default App
